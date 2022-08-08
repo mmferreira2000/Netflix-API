41e54ba9a50debf0a696f7b982bc57e65bf237d4
matheus moreira ferreira <moreira.mferreira@gmail.com>
2022-08-08 19:53:01 -0300

    Setup Versioning

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,2 +1,9 @@
-class ShowsController < ApplicationController
+# frozen_string_literal:true
+
+module Api
+  module V1
+    class ShowsController < ApplicationController
+      
+    end
+  end
 end
