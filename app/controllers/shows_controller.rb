69d62354d8e9299e4abdd2c793bd586b65dae5b9
matheus moreira ferreira <moreira.mferreira@gmail.com>
2022-08-08 20:15:51 -0300

    inplement create endpoint

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal:true
 
-module Api
-  module V1
-    class ShowsController < ApplicationController
-      
+require 'csv'
+
+# comment to avoid RuboCop
+class ShowsController < ApplicationController
+  def create
+    CSV.foreach('db/netflix_titles.csv', headers: :first_row) do |row|
+      @show = Show.create(title: row[2],
+                          genre: row[1],
+                          year: row[7],
+                          country: row[5],
+                          published_at: row[6],
+                          description: row[11])
     end
+    shows = Show.order('year ASC').as_json(except: %i[created_at updated_at])
+    render json: shows
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal:true
 
 Rails.application.routes.draw do
-  namespace 'api' do
-    namespace 'v1' do
-      post 'create' => 'shows#create'
-      get 'shows' => 'shows#index'
-    end
-  end
+  post 'create' => 'shows#create'
+  get 'shows' => 'shows#index'
 end
