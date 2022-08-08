f2d5ebdcede2dd8f0f134f0b386c783e1b494c90
matheus moreira ferreira <moreira.mferreira@gmail.com>
2022-08-08 19:58:40 -0300

    add frozen string literal comment

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
+# frozen_string_literal:true
+
 Rails.application.routes.draw do
-  namespace 'api'do
+  namespace 'api' do
     namespace 'v1' do
-      
+      post 'create' => 'shows#create'
+      get 'shows' => 'shows#index'
     end
   end
 end
